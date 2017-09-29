930d78e4e8cad53c3ec2229cb7796467fa748447
Mikhail Kharitonovich <kharitonovich.m@gmail.com>
2017-09-29 21:37:56 +0300

    Initial commit

---
+++ bot/bot.py
@@ -0,0 +1,50 @@
+import discord
+import asyncio
+from horsebase import HorseBase
+
+client = discord.Client()
+hb = HorseBase()
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+            return
+    if message.content.startswith('!help'):
+        await client.send_message(message.server, '!horse to post a :horse: \n !totalhorses for amout of :horse: posted on this server \n !myhorses to tell how many :horse: you posted \n !tophorses to see who posted the most :horse:')
+    if message.content.startswith('!horse'):
+        await client.send_message(message.server, ':horse:')
+    if message.content.startswith('!totalhorses'):
+        await client.send_message(message.server, 'Total amount of ' + str(hb.getTotalHorses(message.server)) + ' :horse: posted in this channel')
+    if message.content.startswith('!myhorses'):
+        author = (message.author.nick if message.author.nick else message.author.name)
+        myhorses = hb.getMyHorses(message.server, author)
+        await client.send_message(message.server, author + ', you have posted ' + myhorses['month'] + ' :horse: this month and ' + myhorses['total'] + ' :horse: since the beginning of time.')
+    if message.content.startswith('!tophorses'):
+        tophorses = hb.getTopHorses(message.server, 3)
+        if len(tophorses['month']) == 0 and len(tophorses['alltime']) == 0:
+            return
+        message = ""
+        if len(tophorses['month']) > 0:
+            message += "Top posters of this month are: \n"
+            for m in tophorses['month']:
+                message += m[0] + " with " + m[1] + " :horse: \n"
+        if len(tophorses['alltime']) > 0:
+            message += "All time top posters are: \n"
+        for m in tophorses['alltime']:
+                message += m[0] + " with " + m[1] + " :horse: \n"
+        await client.send_message(message.server, message)
+    if '🐴' in message.content or 'horse' in message.content.lower():
+        if '🐴' in message.content:
+            hb.addHorseToDB(message.server, message.timestamp, message.author.nick if message.author.nick else message.author.name)
+        await client.add_reaction(message, '🐴')
+    if 'hi kuubio' in message.content.lower():
+        await client.send_message(message.server, 'HI KUUBIO! :horse:')
+
+client.run('MzYyODg2NTcyNTI3NTgzMjMz.DK7iUQ.ufBzVaQ92XHc4oxUtS-rJD6f7uU')
---
+++ bot/horsebase.py
@@ -0,0 +1,45 @@
+#horsebase = database for storing horses
+from pymongo import MongoClient
+import datetime
+
+class HorseBase:
+    def __init__(self):
+        self.client = MongoClient('localhost', 27017)
+        self.db = self.client['kuubiobot']
+
+    def addHorseToDB(self, channel, time, poster):
+        horse = {'time': time, 'author': poster}
+        self.db[channel].insert_one(horse)
+
+    def getMyHorses(self, channel, poster):
+        total = self.db[channel].find({'author': poster}).count()
+        month = self.db[channel].find({'author': poster, 'time': {'$gte': datetime.date.today().replace(day=1)}}).count()
+        return {'total': total, 'month': month}
+
+    def getTopHorses(self, channel, topPositions):
+        AlltimeTop = []
+        collection = self.db[channel].find()
+        authors = list(set([e['author'] for e in collection]))
+        horsesperauthor = {}
+        for a in authors:
+            horsesperauthor[a] = len([e for e in collection if e['author'] == a])
+        for _ in range(topPositions):
+            topresult = max(horsesperauthor.items(), key=operator.itemgetter(1))
+            AlltimeTop.append(topresult)
+            del horsesperauthor[topresult[0]]
+        
+        MonthTop = []
+        collection = self.db[channel].find({'time': {'$gte': datetime.date.today().replace(day=1)}})
+        authors = list(set([e['author'] for e in collection]))
+        horsesperauthor = {}
+        for a in authors:
+            horsesperauthor[a] = len([e for e in collection if e['author'] == a])
+        for _ in range(topPositions):
+            topresult = max(horsesperauthor.items(), key=operator.itemgetter(1))
+            MonthTop.append(topresult)
+            del horsesperauthor[topresult[0]]
+
+        return {'alltime': AlltimeTop, 'month': MonthTop}
+
+    def getTotalHorses(self, channel):
+        return self.db[channel].find().count()
