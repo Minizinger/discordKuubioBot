f2355598c8e64436921b18091a93b716fe87670e
Mikhail Kharitonovich <kharitonovich.m@gmail.com>
2017-09-30 02:10:58 +0300

    date and docker compose fixes

--- bot/horsebase.py
+++ bot/horsebase.py
@@ -13,7 +13,7 @@ class HorseBase:
 
     def getMyHorses(self, channel, poster):
         total = self.db[channel].find({'author': poster}).count()
-        month = self.db[channel].find({'author': poster, 'time': {'$gte': datetime.date.today().replace(day=1)}}).count()
+        month = self.db[channel].find({'author': poster, 'time': {'$gte': datetime.datetime.today().replace(day=1, hour=0, minute=0, second=0)}}).count()
         return {'total': total, 'month': month}
 
     def getTopHorses(self, channel, topPositions):
@@ -30,7 +30,7 @@ class HorseBase:
                 del horsesperauthor[topresult[0]]
         
         MonthTop = []
-        collection = self.db[channel].find({'time': {'$gte': datetime.date.today().replace(day=1)}})
+        collection = self.db[channel].find({'time': {'$gte': datetime.datetime.today().replace(day=1, hour=0, minute=0, second=0)}})
         authors = list(set([e['author'] for e in collection]))
         horsesperauthor = {}
         for a in authors:
