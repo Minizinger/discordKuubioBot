488d63ced84ad3fe6dcd80cded5a89308e6e7666
Mikhail Kharitonovich <kharitonovich.m@gmail.com>
2017-10-01 22:21:48 +0300

    fixed set

--- external_data/load_data.py
+++ external_data/load_data.py
@@ -5,8 +5,8 @@ from pymongo import MongoClient
 with MongoClient(os.environ.get('MONGO_ADDRESS')) as client:
     with open('reactions.json') as reactions:
         reactions = json.load(reactions)
-        client['kb_dynamic']['reactions'].update_one({"$set": {"name": "reactions"}}, reactions, True)
+        client['kb_dynamic']['reactions'].update_one({"name": "reactions"}, {"$set": reactions}, True)
 
     with open('responses.json') as responses:
         responses = json.load(responses)
-        client['kb_dynamic']['responses'].update_one({"$set": {"name": "responses"}}, responses, True)
+        client['kb_dynamic']['responses'].update_one({"name": "responses"}, {"$set": responses}, True)
