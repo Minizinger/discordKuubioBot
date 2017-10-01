346c3b02ba104b655f27d175c36093947b3ec45b
Mikhail Kharitonovich <kharitonovich.m@gmail.com>
2017-10-01 20:39:34 +0300

    fixed database use

--- external_data/load_data.py
+++ external_data/load_data.py
@@ -5,8 +5,8 @@ from pymongo import MongoClient
 with MongoClient(os.environ.get('MONGO_ADDRESS')) as client:
     with open('reactions.json') as reactions:
         reactions = json.load(reactions)
-        client['kb_dynamic']['reactions'] = reactions
+        client['kb_dynamic']['reactions'].updateOne({"name": "reactions"}, reactions, True)
 
     with open('responses.json') as responses:
         responses = json.load(responses)
-        client['kb_dynamic']['responses'] = responses
+        client['kb_dynamic']['responses'].updateOne({"name": "responses"}, responses, True)
