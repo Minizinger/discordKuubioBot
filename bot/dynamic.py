769073dae73adf80b321dcc3148dc72ecb5fa708
Mikhail Kharitonovich <kharitonovich.m@gmail.com>
2017-10-01 22:32:10 +0300

    fixed database calls

--- bot/dynamic.py
+++ bot/dynamic.py
@@ -7,12 +7,12 @@ class Dynamic:
         self.db = self.client['kb_dynamic']
     
     def determineReactions(self, message):
-        reactions = self.db['reactions'].find_one({"name": "reactions"}, {"name": 0})
+        reactions = self.db['reactions'].find_one({"name": "reactions"}, {"name": 0, "_id": 0})
         return [reaction for reaction in reactions if any(r in message for r in reactions[reaction])]
     
     # only returning one responce here to prevent 
     def determineResponse(self, message):
-        responses = self.db['responses'].find_one({"name": "responses"}, {"name": 0})
+        responses = self.db['responses'].find_one({"name": "responses"}, {"name": 0, "_id": 0})
         start = [response for response in responses['start'] if any(r in message for r in responses['start'][response])]
         if start:
             return start[0]
