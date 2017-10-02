6fd7e1915825a687e6f2876cf7349db021817540
Mikhail Kharitonovich <kharitonovich.m@gmail.com>
2017-10-02 20:41:33 +0300

    only match distinct words

---
+++ README.md
@@ -0,0 +1,10 @@
+## Start
+```docker-compose build
+docker-compose up -d```
+
+docker-compose.yml contains mongo image for ARM (raspberry pi), to start it on normal kerner replace image with mongo:latest
+
+Also don't forget to rename example.env to .env and add your data there
+
+## Import data
+```docker compose exec bot python3 ../load_data.py```
--- bot/dynamic.py
+++ bot/dynamic.py
@@ -1,5 +1,6 @@
 from pymongo import MongoClient
 import os
+import re
 
 class Dynamic:
     def __init__(self):
@@ -8,12 +9,12 @@ class Dynamic:
     
     def determineReactions(self, message):
         reactions = self.db['reactions'].find_one({"name": "reactions"}, {"name": 0, "_id": 0})
-        return [reaction for reaction in reactions if any(r in message for r in reactions[reaction])]
+        return [reaction for reaction in reactions if any(re.search(r'\b' + r + r'\b', message) for r in reactions[reaction])]
     
     # only returning one responce here to prevent 
     def determineResponse(self, message):
         responses = self.db['responses'].find_one({"name": "responses"}, {"name": 0, "_id": 0})
-        start = [response for response in responses['start'] if any(r in message for r in responses['start'][response])]
+        start = [response for response in responses['start'] if any(re.search(r'\b' + r + r'\b', message) for r in responses['start'][response])]
         if start:
             return start[0]
         other = [response for response in responses['any'] if any(r in message for r in responses['any'][response])]
